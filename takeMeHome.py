8eda279da493c19f82afcce31b88cb3e4562da64
bensnell <bensnellstudio@gmail.com>
2018-08-01 13:43:14 -0400

    Changes

--- takeMeHome.py
+++ takeMeHome.py
@@ -279,7 +279,7 @@ def scrapeCL():
 	allApts = []
 
 	# Iterate through all possible apartments
-	maxSearch = 3000
+	maxSearch = 1000
 	for i in range(0, maxSearch, 120):
 
 		print("Got CL Apartments " + str(i) + " / " + str(maxSearch))
